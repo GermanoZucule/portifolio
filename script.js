781db9ac81f59843344cb7168e7e6245ced40b75
GermanoZucule <gr.zucule@gmail.com>
2025-02-03 11:24:17 +0100

    medidas para mobile

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+// Menu hamburguer
+const menu = document.querySelector("#menu");
+const hbr = document.querySelector(".hamburguer");
+
+menu.addEventListener("click", () => {
+  hbr.classList.toggle("hidden");
+});
+
+// onload
+window.addEventListener(
+  "load",
+  window.scrollTo({
+    top: 0,
+  })
+);
+
 // Eventos de escrolagem
 window.addEventListener("scroll", () => {
   const header = window.document.querySelector(".nome");
   header.classList.toggle("rolagem", scrollY > 250);
   const head = window.document.querySelector(".cabeca");
   head.classList.toggle("roll", scrollY > 100);
+  hbr.classList.toggle("hidden", scrollY > 0);
 });
 
 // Evento de escrolagem apos o click
